9f3acb0148f484b7cc3ff853855fb3cbc6baa00e
Daria Lastname <minzulko@gmail.com>
2023-07-27 17:41:32 +0300

    initial commit

---
+++ src/main/java/web/service/UserService.java
@@ -0,0 +1,13 @@
+package web.service;
+
+import web.models.User;
+
+import java.util.List;
+
+public interface UserService {
+    List<User> getAllUsers();
+    User getUserById(int id);
+    void saveUser(User user);
+    void updateUser(int id, User user);
+    void deleteUser (int id);
+}
